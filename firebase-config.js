6f4a4be446ec3b315f6c8de3e23c5e37218fe189
Pascal-TP <pascal.gasch@tpholding.de>
2026-04-02 14:24:51 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -8,3 +8,13 @@ export const firebaseConfig = {
   appId: "1:266297890825:web:6fc9f9948475a596c380d8",
   measurementId: "G-JS80PZXD5N"
 };
+
+export const blazeConfig = {
+  apiKey: "AIzaSyCcHI5sGR7sFwrWRpo2uQ3Plm0HpTvqr30",
+  authDomain: "kalkpro-4cc29.firebaseapp.com",
+  projectId: "kalkpro-4cc29",
+  storageBucket: "kalkpro-4cc29.firebasestorage.app",
+  messagingSenderId: "185447466021",
+  appId: "1:185447466021:web:ed1e448d9b92e151b52bcc",
+  measurementId: "G-K594BLG0G4"
+};
